a386be931658bd1cf48c3faae11af74cbbfa7625
Asqarbek Olimov <88034288+AsqarbekOlimov@users.noreply.github.com>
2022-07-05 09:10:04 +0500

    Sent message telegram

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function submit({token, chatId, data}){
-    fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${data}`)
+function submit({ token, chatId, data }) {
+  fetch(
+    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${data}`,
+  )
 }
 
-document.querySelector('form').addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const name = document.querySelector("[name=name]").value;
-    const phone = document.querySelector("[name=phone]").value;
-    const msg = document.querySelector("[name=msg]").value;
+document.querySelector('form').addEventListener('submit', (e) => {
+  //   e.preventDefault()
+  const name = document.querySelector('[name=name]').value
+  const phone = document.querySelector('[name=phone]').value
+  const msg = document.querySelector('[name=msg]').value
 
-    if(name !=='' && phone !== '' && msg !== '')
-        submit({
-            token: "5104059461:AAE9gSrdGMopN1QyrMYHOm6irD3ZYOnQp7k",
-            chatId: "1637157234",
-            data: `Ismi: ${name} %0ATelefon raqami: ${phone}%0A Xabar: ${msg}`
-        })
+  if (name !== '' && phone !== '' && msg !== '')
+    submit({
+      token: '5104059461:AAE9gSrdGMopN1QyrMYHOm6irD3ZYOnQp7k',
+      chatId: '1637157234',
+      data: `Ismi: ${name} %0ATelefon raqami: ${phone}%0A Xabar: ${msg}`,
+    })
 })
